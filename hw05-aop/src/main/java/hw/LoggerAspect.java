eacaed46caf630ac6873d6749bb57931f0efa8b2
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-06-16 16:13:48 +0300

    добавил кеш для методов и заменил интерфейс на тип T

--- hw05-aop/src/main/java/hw/LoggerAspect.java
+++ hw05-aop/src/main/java/hw/LoggerAspect.java
@@ -3,29 +3,44 @@ package hw;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
-public class LoggerAspect implements InvocationHandler {
-    private final TestLoggingInterface testLoggingInterface;
+public class LoggerAspect<T> implements InvocationHandler {
+    private final T target;
+    private final Map<Method, Boolean> logAnnotatedMethods = new HashMap<>();
 
-    public LoggerAspect(TestLoggingInterface testLoggingInterface) {
-        this.testLoggingInterface = testLoggingInterface;
+    public LoggerAspect(T target) {
+        this.target = target;
+
+        for (Method method : target.getClass().getInterfaces()[0].getMethods()) {
+            try {
+                Method realMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
+                logAnnotatedMethods.put(method, realMethod.isAnnotationPresent(Log.class));
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        Method realMethod = testLoggingInterface.getClass().getMethod(method.getName(), method.getParameterTypes());
-        if (realMethod.isAnnotationPresent(Log.class)) {
+        if (logAnnotatedMethods.getOrDefault(method, false)) {
             if (args != null) {
                 for (Object arg : args) {
                     System.out.println(arg);
                 }
             }
         }
-        return method.invoke(testLoggingInterface, args);
+        return method.invoke(target, args);
     }
 
-    public static TestLoggingInterface createProxy(TestLoggingInterface testLoggingInterface) {
-        return (TestLoggingInterface) Proxy.newProxyInstance(LoggerAspect.class.getClassLoader(),
-                new Class<?>[]{TestLoggingInterface.class}, new LoggerAspect(testLoggingInterface));
+    public static <T> T createProxy(T target) {
+        Class<?>[] interfaces = target.getClass().getInterfaces();
+        return (T) Proxy.newProxyInstance(
+                target.getClass().getClassLoader(),
+                interfaces,
+                new LoggerAspect<>(target)
+        );
     }
 }
